d8c7dd714ccdf19a5e3e797124ecc940712e4fdb
ascariis <albertoporcu4@gmail.com>
2022-12-29 17:44:28 +0100

    Prettify the scripts a little

--- setwall.sh
+++ setwall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-feh --bg-fill /home/alberto/Downloads/wallhaven-9mjoy1_1920x1080.png
+feh --bg-fill $HOME/Downloads/wallhaven-9mjoy1_1920x1080.png
--- startwm.sh
+++ startwm.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #
 
-xrandr --output HDMI-A-1 --set TearFree on
-setxkbmap it &
-./setwall.sh &
-dwmblocks &
+xrandr --output HDMI-A-1 --set TearFree on # Removes screen tearing
+setxkbmap it & # Sets keyboard layout to italian
+./setwall.sh & # Calls script to set the wallpaper
+dwmblocks & # Starts statusbar
 exec dwm
